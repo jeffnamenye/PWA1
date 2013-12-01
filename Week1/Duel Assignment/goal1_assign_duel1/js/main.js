f403fea07ddd56bc3cbd1a11208bf73187e0c971
Jeff Namenye <jeffnamenye@hotmail.com>
2013-11-30 20:33:44 -0500

    Second commit

--- Week1/Duel Assignment/goal1_assign_duel1/js/main.js
+++ Week1/Duel Assignment/goal1_assign_duel1/js/main.js
@@ -6,7 +6,38 @@
  //self-executing function
  (function(){
  	
- 	
  	console.log("Fight!!!");
  	
+ 	//player name
+ 	var playerOneName ="Spiderman";
+ 	var playerTwoName = "Batman";
+ 	
+ 	//player damage
+ 	var playerOneDamage = 20;
+ 	var playerTwoDamage = 20;
+ 	
+ 	//player health
+ 	var playerOneHealth = 100;
+ 	var playerTwoHealth = 100;
+ 	
+ 	var round = 1;
+ 	
+ 	//fight function
+ 	function fight () {
+ 		
+ 		console.log ("in the fight function");
+ 		
+	   
+	 };
+	 //funtion to check for winner
+	function winnerCheck(){
+		//code will go here
+		
+	};
+	
+	/******The program gets started below******/
+	
+	console.log ("program starts");
+	fight();
+	
  })();
